b386717b7121a8617b97476afb6560c74e272dee
NickChow <nickchow.stretch@gmail.com>
2019-06-24 15:14:00 -0400

    add GOPATH/GOROOT

--- path.zsh
+++ path.zsh
@@ -3,3 +3,9 @@ export PATH="$HOME/.node/bin:$PATH"
 
 # Use project specific binaries before global ones
 export PATH="node_modules/.bin:vendor/bin:$PATH"
+
+# Export GOPATH & GOROOT 
+export GOPATH=$HOME/go
+export GOROOT=/usr/local/opt/go/libexec
+export PATH=$PATH:$GOPATH/bin
+export PATH=$PATH:$GOROOT/bin
