b90750b152a0c529a51e2beecf92173237b5693f
NickChow <nickchow.stretch@gmail.com>
2019-07-02 13:15:17 -0400

    add python to path

--- path.zsh
+++ path.zsh
@@ -4,6 +4,9 @@ export PATH="$HOME/.node/bin:$PATH"
 # Use project specific binaries before global ones
 export PATH="node_modules/.bin:vendor/bin:$PATH"
 
+# Add python to PATH
+export PATH="usr/local/share/python:$PATH"
+
 # Export GOPATH & GOROOT 
 export GOPATH=$HOME/go
 export GOROOT=/usr/local/opt/go/libexec
