f13dcf81abe49282a2818e236cf9a9889531b299
NickChow <nickchow.stretch@gmail.com>
2020-09-18 09:39:07 -0700

    update powerlevel9k

--- powerlevel9k.zsh
+++ powerlevel9k.zsh
@@ -1,7 +1,7 @@
 # For configuration details, look here:
 # https://github.com/Powerlevel9k/powerlevel9k
 
-POWERLEVEL9K_SHORTEN_DIR_LENGTH=2
+POWERLEVEL9K_SHORTEN_DIR_LENGTH=4
 POWERLEVEL9K_SHORTEN_STRATEGY="truncate_from_right"
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context aws kubecontext newline dir vcs newline)
 #POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(newline dir vcs)
